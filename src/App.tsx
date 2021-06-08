e5c932001020041b7d7a35206e60e48c5a60b5a0
Heath Daniel <mr.heath.daniel@gmail.com>
2021-06-08 16:11:18 -0400

    Add react-chat-engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
 import logo from './logo.svg';
 import './App.css';
 
+const { REACT_APP_PUBLIC_KEY, REACT_APP_USERNAME, REACT_APP_PASSWORD } = process.env;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      publicKey={REACT_APP_PUBLIC_KEY}
+      userName={REACT_APP_USERNAME}
+      userPassword={REACT_APP_PASSWORD}
+    />
   );
 }
 
